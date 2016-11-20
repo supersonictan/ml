e8bbb786cd19b602de22dddbafb77d18d50856bc
谈圳 <TanZhen@tanzhendeMacBook-Pro.local>
2016-11-21 00:13:20 +0800

    init ftrl

---
+++ src/main/java/com/priv/tanzhen/ml/model/IModel.java
@@ -0,0 +1,14 @@
+package com.priv.tanzhen.ml.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by TanZhen on 2016/11/20.
+ */
+public interface IModel extends Serializable {
+
+
+    public void train(double[] example);
+    public double score(double[] inst);
+    public String debug();
+}
